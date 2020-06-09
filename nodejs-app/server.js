924b3bb35c7718a0099f8159b1dedb05c799a000
eblache <student@workstation.lab.example.com>
2020-06-09 13:22:32 +0000

    Updated environment variable

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,8 +4,12 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
 });
+app.get('/test', function (req, res) {
+  res.send('Test Hello World Success\n')
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
